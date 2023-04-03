0e2a879e5ef8d841abcaba57c78b1d3f6ab1c52a
Frank Anneveld <frank@anneveld.it>
2023-04-03 17:20:11 +0200

    Added books demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Book, HttpService } from './services/http/http.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngmocks-jest';
+export class AppComponent implements OnInit {
+  public httpService = inject(HttpService);
+
+  public books: Observable<Book[]> = new Observable<Book[]>();
+
+  ngOnInit(): void {
+    this.books = this.httpService.getBooks();
+  }
 }
---
+++ src/app/services/http/http.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+export type Book = {
+  book_id: number;
+  book_title: string;
+  author_name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private http = inject(HttpClient);
+  private booksUrl: string = './assets/books.json';
+
+  public getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.booksUrl).pipe(
+      map((resp: any) => resp.books)
+    );
+  }
+}
