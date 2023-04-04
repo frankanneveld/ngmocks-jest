f52d0597a09f58acc44e18bb88e9abee611bdd32
Anneveld, Frank (AV) <FAnneveld@svb.nl>
2023-04-04 20:28:17 +0200

    Added Arrays

--- src/app/services/http/http.service.spec.ts
+++ src/app/services/http/http.service.spec.ts
@@ -54,27 +54,5 @@ describe('HttpService', () => {
     expect(httpClient.get).toHaveBeenCalledTimes(1);
   });
 
-  it('should do something', () => {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const obs$ = from(arr).pipe(map((r) => r + 1));
-    const expected = 55;
 
-    let count = 0;
-    obs$.subscribe((res) => (count += res));
-
-    expect(count).toEqual(expected);
-    expect(count).toBe(expected);
-    expect(count).not.toBeNull();
-  });
-
-  it('should do something with array subscribtion', () => {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const obs$ = from(arr);
-
-    obs$.subscribe((res) => {
-      console.log(res, res === 1);
-      expect(res).not.toBe(56)
-    });
-
-  });
 });
