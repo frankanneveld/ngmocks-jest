092d5305dabb250527dd0e30e95f16567ae07c3b
Frank Anneveld <frank@anneveld.it>
2023-04-04 20:20:40 +0200

    extra test

--- src/app/services/http/http.service.spec.ts
+++ src/app/services/http/http.service.spec.ts
@@ -1,17 +1,13 @@
-import { HttpClient } from '@angular/common/http';
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
+import { delay, from, map, of, tap } from 'rxjs';
 import { HttpService } from './http.service';
-import { of } from 'rxjs';
-import { MockProvider } from 'ng-mocks';
 
 describe('HttpService', () => {
   let service: HttpService;
-  let httpController: HttpTestingController;
-  // let httpClient: HttpClient;
+  // let httpController: HttpTestingController;
+  let httpClient: HttpClient;
 
   const books = {
     books: [
@@ -42,34 +38,43 @@ describe('HttpService', () => {
     });
 
     service = TestBed.inject(HttpService);
-    httpController = TestBed.inject(HttpTestingController);
-    // httpClient = TestBed.inject(HttpClient);
+    // httpController = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
   });
-
-  // afterEach(() => {
-  //   httpController.verify();
-  //   console.log('afterEach');
-  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   it('should get books', () => {
-    // jest.spyOn(service, 'getBooks').mockReturnValue(of(books.books));
+    jest.spyOn(httpClient, 'get').mockReturnValue(of(books));
+    service.getBooks().subscribe();
+    expect(httpClient.get).toHaveBeenCalled();
+    expect(httpClient.get).toHaveBeenCalledTimes(1);
+    expect(httpClient.get).toHaveBeenCalledTimes(1);
+  });
 
-    service.getBooks().subscribe((res) => {
-      expect(res).toEqual(books);
-      expect(false).toBeTruthy(); // this will pass the test also ????
-      console.log(res);
+  it('should do something', () => {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const obs$ = from(arr).pipe(map((r) => r + 1));
+    const expected = 55;
+
+    let count = 0;
+    obs$.subscribe((res) => (count += res));
+
+    expect(count).toEqual(expected);
+    expect(count).toBe(expected);
+    expect(count).not.toBeNull();
+  });
+
+  it('should do something with array subscribtion', () => {
+    const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const obs$ = from(arr);
+
+    obs$.subscribe((res) => {
+      console.log(res, res === 1);
+      expect(res).not.toBe(56)
     });
 
-    // const req = httpController.expectOne({
-    //   method: 'GET',
-    //   url: './assets/books.json',
-    // });
-    // console.log(req);
-    // expect(req.request.method).toBe('GET');
-    // req.flush(books);
   });
 });
