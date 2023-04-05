2a4761c54075334101d1ebc3fffb1849789c717f
Anneveld, Frank (AV) <FAnneveld@svb.nl>
2023-04-05 22:06:03 +0200

    WIP

--- src/app/interceptors/http-book.interceptor.ts
+++ src/app/interceptors/http-book.interceptor.ts
@@ -3,20 +3,39 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HTTP_INTERCEPTORS,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class HttpBookInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      tap(console.log)
-    );
-  }
+  // intercept(
+  //   request: HttpRequest<unknown>,
+  //   next: HttpHandler
+  // ): Observable<HttpEvent<unknown>> {
+  //   return next.handle(request).pipe(tap(console.log));
+  // }
 
-  
+  public intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (req?.url.includes('error')) return next.handle(req);
+    return next.handle(req).pipe(catchError((error) => this.handleError(error, req)));
 }
+
+private handleError(error: HttpErrorResponse, req: HttpRequest<unknown>): Observable<never> {
+    const errorMessage = `HTTP_ERROR: Call ${req.method} to ${req.url} has failed.`;
+    console.error(error, errorMessage);
+    return throwError(() => ({error, errorMessage}));
+}
+}
+
+export const HttpBookInterceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: HttpBookInterceptor,
+    multi: true,
+  },
+];
