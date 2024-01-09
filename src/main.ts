024d6a396837d8ea051ba068e764980680f52fdc
Frank Anneveld <frank@anneveld.it>
2024-01-09 13:21:45 +0100

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpBookInterceptorProvider } from './app/interceptors/http-book.interceptor';
 
-// import { AppModule } from './app/app.module';
-
-
-// platformBrowserDynamic().bootstrapModule(AppModule)
-  
-
-  bootstrapApplication(AppComponent, {
-    providers: [HttpBookInterceptorProvider]
-  }).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [HttpBookInterceptorProvider],
+}).catch((err) => console.error(err));
